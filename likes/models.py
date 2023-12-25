e7a0854127b803a872b5c1bf3e450449ea8d9e4e
Milos Tasic <tasic29@gmail.com>
2023-12-25 15:06:44 +0100

    Setting Up the Database

--- likes/models.py
+++ likes/models.py
@@ -7,5 +7,5 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class LikedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveSmallIntegerField()
-    content_object = models.ForeignKey(GenericForeignKey)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
